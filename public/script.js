b1a6dd93740159525e16c80973bb071b2be8f7da
Davide Bianchin <bianchindavide@itis-molinari.eu>
2025-01-29 17:56:55 +0100

    creazione render

--- public/script.js
+++ public/script.js
@@ -1,157 +1,130 @@
-
 let todos = [];
 
 const render = () => {
+    const todoList = document.getElementById('todoList');
+    todoList.innerHTML = ''; 
+  
+ todos.forEach((todo, index) => 
+  todoList.innerHTML += 
+    "<li class='todo-item " + (todo.completed ? 'completed-box' : '') + "'>" +
+      "<span class='task-name'>" + todo.name + "</span>" +
+      "<button class='complete-btn'>Complete</button>" +
+      "<button class='delete-btn'>Delete</button>" +
+    "</li>"
+    );
 
-   
-
-}
+  
+    
+    const completeButtons = document.querySelectorAll('.complete-btn');
+    const deleteButtons = document.querySelectorAll('.delete-btn');
+  
+    
+    completeButtons.forEach((button, index) => {
+      button.onclick = () => {
+        todos[index].completed = !todos[index].completed; 
+        completeTodo(todos[index]).then(() => render()); 
+      };
+    });
+  
+    
+    deleteButtons.forEach((button, index) => {
+      button.onclick = () => {
+        deleteTodo(todos[index].id).then(() => {
+          todos.splice(index, 1); 
+          render(); 
+        });
+      };
+    });
+};
+  
 
 const send = (todo) => {
-
-    return new Promise((resolve, reject) => {
- 
-       fetch("/todo/add", {
- 
-          method: 'POST',
- 
-          headers: {
- 
-             "Content-Type": "application/json"
- 
-          },
- 
-          body: JSON.stringify(todo)
- 
-       })
- 
-       .then((response) => response.json())
- 
-       .then((json) => {
- 
-          resolve(json);
- 
-       })
- 
+  return new Promise((resolve, reject) => {
+    fetch("/todo/add", {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(todo)
     })
- 
- }
- 
- const load = () => {
- 
-    return new Promise((resolve, reject) => {
- 
-       fetch("/todo")
- 
-       .then((response) => response.json())
- 
-       .then((json) => {
- 
-          resolve(json);
- 
-       })
- 
-    })
- 
- }
-
-
- insertButton.onclick = () => {
-
-    const todo = {          
- 
-       name: todoInput.value,
- 
-       completed: false
- 
-    }      
- 
-    send({todo: todo})
- 
-     .then(() => load())
- 
-     .then((json) => { 
- 
-       todos = json.todos;
- 
-       todoInput.value = "";
- 
-       render(); 
- 
+    .then((response) => response.json())
+    .then((json) => {
+      resolve(json);
     });
- 
- }
+  });
+};
 
-
- const completeTodo = (todo) => {
-
-    return new Promise((resolve, reject) => {
- 
-       fetch("/todo/complete", {
- 
-          method: 'PUT',
- 
-          headers: {
- 
-             "Content-Type": "application/json"
- 
-          },
- 
-          body: JSON.stringify(todo)
- 
-       })
- 
-       .then((response) => response.json())
- 
-       .then((json) => {
- 
-          resolve(json);
- 
-       })
- 
-    })
- 
- }
-
-
- const deleteTodo = (id) => {
-
-    return new Promise((resolve, reject) => {
- 
-       fetch("/todo/"+id, {
- 
-          method: 'DELETE',
- 
-          headers: {
- 
-             "Content-Type": "application/json"
- 
-          },
- 
-       })
- 
-       .then((response) => response.json())
- 
-       .then((json) => {
- 
-          resolve(json);
- 
-       })
- 
-    })
- 
- }
-
- setInterval(() => {
-
-    load().then((json) => {
- 
-       todos = json.todos;
- 
-       todoInput.value = "";
- 
-       render();
- 
+const load = () => {
+  return new Promise((resolve, reject) => {
+    fetch("/todo")
+    .then((response) => response.json())
+    .then((json) => {
+      resolve(json);
     });
+  });
+};
+
+insertButton.onclick = () => {
+  const todo = {          
+    name: todoInput.value,
+    completed: false
+  };      
+
+  send({todo: todo})
+    .then(() => load())
+    .then((json) => { 
+      todos = json.todos;
+      todoInput.value = "";
+      render(); 
+  });
+};
+
+load().then((json) => {
+
+    todos = json.todos;
  
- }, 30000);
+    render();
+ 
+ });
+ 
+
+const completeTodo = (todo) => {
+  return new Promise((resolve, reject) => {
+    fetch("/todo/complete", {
+      method: 'PUT',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(todo)
+    })
+    .then((response) => response.json())
+    .then((json) => {
+      resolve(json);
+    });
+  });
+};
+
+const deleteTodo = (id) => {
+  return new Promise((resolve, reject) => {
+    fetch("/todo/" + id, {
+      method: 'DELETE',
+      headers: {
+        "Content-Type": "application/json"
+      },
+    })
+    .then((response) => response.json())
+    .then((json) => {
+      resolve(json);
+    });
+  });
+};
+
+setInterval(() => {
+   
+  load().then((json) => {
+    todos = json.todos;
+    todoInput.value = "";
+    render();
+  });
+}, 30000);
+
